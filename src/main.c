30d78a92130be37f8a69a9049335637997db0e08
acitate <m.parsa.kolivand@gmail.com>
2026-08-02 23:27:08 +0330

    refactor: Move application entry point and main loop from `server.c` to `main.c`
    - Moved main loop to `main.c` because `server.c` will serve as the main orchestrator,
    - Each received connection will now spawn a thread.

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include "network.h"
+#include "server.h"
+#define PORT 8080
+#define BUFFER_SIZE 65536
+
+
+static void *handle_connection(void *arg)
+{   
+    int client_fd = (int)(intptr_t) arg;
+    char buffer[BUFFER_SIZE];
+    size_t n = read_request(client_fd, buffer, sizeof(buffer));
+
+    serve_request(buffer);
+
+    network_close(client_fd);
+}
+
+
+int main()
+{
+    int server_fd = get_server_socket(8080);
+    while (1) {
+        int client_fd = network_accept(server_fd);
+        pthread_t tid;
+        pthread_create(&tid, NULL, handle_connection, (void *)(intptr_t) client_fd);
+    }
+}
