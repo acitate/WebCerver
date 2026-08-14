494d7db7cac1666e973e9dda0a74ed73c83caf0c
acitate <m.parsa.kolivand@gmail.com>
2026-08-14 23:17:11 +0330

    feat(server): add (PathStatus, HttpParseStatus) -> HTTP response code mapping functions
    `http_code_for_PathStatus` function maps resource resolving status to HTTP response code.
    `http_code_for_HttpParseStatus` function maps request parsing status to HTTP response code.

--- src/server/server.c
+++ src/server/server.c
@@ -14,3 +14,39 @@ void server_process_request(const char *raw, size_t len, char **response, size_t
     HttpRequest request;
     http_parse_request(request_str, len, &request);
 }
+
+
+static int http_code_for_ParseStatus(HttpParseStatus pstatus) 
+{
+    switch (pstatus) {
+        case HTTP_PARSE_ERR_MALFORMED_REQUEST_LINE: return 400;
+        case HTTP_PARSE_ERR_URI_TOO_LONG: return 414;
+        case HTTP_PARSE_ERR_UNSUPPORTED_VERSION: return 505;
+        case HTTP_PARSE_ERR_MALFORMED_HEADER: return 400;
+        case HTTP_PARSE_ERR_HEADER_TOO_LONG: return 431;
+        case HTTP_PARSE_ERR_TOO_MANY_HEADERS: return 431;
+        case HTTP_PARSE_ERR_BUFFER_OVERRUN: return 500;
+        case HTTP_PARSE_OK:
+        case HTTP_PARSE_INCOMPLETE:
+        default: return 500;
+    }
+}
+
+
+static int http_code_for_PathStatus(PathStatus pstatus) 
+{
+    switch (pstatus)
+    {
+    case PATH_ERR_MALFORMED_ENCODING: 
+    case PATH_ERR_NULL_BYTE:
+    case PATH_ERR_CONTROL_CHAR: return 400;
+    case PATH_ERR_TOO_LONG: return 414;
+    case PATH_ERR_NOT_FOUND: return 404;
+    case PATH_ERR_HIDDEN_PATH: return 404;
+    case PATH_ERR_FORBIDDEN: return 404; // Not 403 in order to keep server data hidden
+    case PATH_ERR_IS_DIRECTORY: return 403;
+    case PATH_ERR_UNEXPECTED: return 500;
+    case PATH_OK:
+    default: return 500;
+    }
+}
